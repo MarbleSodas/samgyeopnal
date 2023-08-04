2b020f65f539655d21f16aa2044662d2dca25d5a
Eugene Park <eugenepark404@gmail.com>
2023-08-03 20:44:26 -0700

    Added image upload to posts
    used axios to attempt to upload images to google photos, failed due to blockage by CORS policy

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,42 @@
+'use client';
+
 import Link from 'next/link'
+import { useSession } from 'next-auth/react'
+import axios from 'axios';
 
 export default function Form ({ type, post, setPost, submitting, handleSubmit }){
+    const { data: session } = useSession();
+
+    const handleFileSelected = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+          const reader = new FileReader();
+          reader.onloadend = async () => {
+            const imageBase64 = reader.result;
+            try {
+              const response = await axios.post(
+                'https://photoslibrary.googleapis.com/v1/uploads',
+                imageBase64,
+                {
+                  headers: {
+                    Authorization: `Bearer ${session.accessToken}`,
+                    'Content-type': 'application/octet-stream',
+                    'X-Goog-Upload-Content-Type': file.type,
+                    'X-Goog-Upload-Protocol': 'raw',
+                  },
+                }
+              );
+              const uploadToken = response.data;
+              console.log('uploadToken', uploadToken);
+              setPost({ ...post, image: uploadToken });
+            } catch (error) {
+              console.error('Error uploading image to Google Photos API', error);
+            }
+          };
+          reader.readAsDataURL(file);
+        }
+      };
+
         return (
             <section className='w-full max-w-full flex-start flex-col'>
                 <h1 className='head_text text-left'>
@@ -15,6 +51,14 @@ export default function Form ({ type, post, setPost, submitting, handleSubmit })
                 className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'
                 >
                     {/* Figure Out How to use images in the post */}
+                    <label>
+                        <span className='font-satoshi font-semibold text-base text-gray-700'>
+                            Image
+                        </span>
+                        <input type='file' id='imageInput' name='image' onChange={handleFileSelected}/>
+                    </label>
+
+                    {post.image && <img src={post.image} alt="Preview" />}
 
                     <label>
                         <span className='font-satoshi font-semibold text-base text-gray-700'>
