f27621e93ff13f3dd0d8585835e49f232334c2b0
Eugene Park <eugenepark404@gmail.com>
2023-08-04 17:46:08 -0700

    Update Dropimage.jsx

--- src/components/Dropimage.jsx
+++ src/components/Dropimage.jsx
@@ -1,43 +1,18 @@
-'use client'
-
 import { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
-import axios from 'axios';
 import { useSession } from "next-auth/react";
 import Image from 'next/image';
-import NextCors from 'nextjs-cors';
 
-const Dropimage = async ({ className, post, setPost }) => {
+const Dropimage = ({ className, post, setPost }) => {
     const { data: session } = useSession();
+    const [file, setFile] = useState([]);
 
     const onDrop = useCallback(acceptedFiles => {
         console.log('acceptedFiles', acceptedFiles);
         if (acceptedFiles?.length) {
-            const file = acceptedFiles[0];
-            const reader = new FileReader();
-            reader.onloadend = async () => {
-                const imageBase64 = reader.result;
-                try {
-                const response = await axios.post(
-                    'https://photoslibrary.googleapis.com/v1/uploads',
-                    imageBase64,
-                    {
-                    headers: {
-                        Authorization: `Bearer ${session.accessToken}`,
-                        'Content-type': 'application/octet-stream',
-                        'X-Goog-Upload-Content-Type': file.type,
-                        'X-Goog-Upload-Protocol': 'raw',
-                    },
-                    }
-                );
-                const uploadToken = response.data;
-                console.log('uploadToken', uploadToken);
-                setPost({ ...post, image: uploadToken, imageRaw: imageBase64 });
-                } catch (error) {
-                console.error('Error uploading image to Google Photos API', error);
-                }
-            };
-            reader.readAsDataURL(file);
+            setFile(acceptedFiles[0]);
+            Object.assign(file, {preview: URL.createObjectURL(file)});
+            console.log(file.preview);
         }
       }, [])
       const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
@@ -52,9 +27,9 @@ const Dropimage = async ({ className, post, setPost }) => {
                 <p>Drag 'n' drop some files here, or click to select files</p>
             }
             </div>
-            {post.imageRaw && (
-            <div>
-                <Image alt="" src={post.imageRaw}></Image>
+            {file.name && (
+            <div className="text-center">
+                <Image alt={file.name} src={file.preview} width={100} height={100}></Image>
                 <p>{file.name}</p>
             </div>
             )}
